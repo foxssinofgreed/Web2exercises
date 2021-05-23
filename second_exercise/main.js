e2e34baba29b2d8a4ee0dce97b8c11b88fcb9c56
fox <l_aptsiauri@cu.edu.ge>
2021-05-23 23:53:50 +0400

    Unfinished code

--- second_exercise/main.js
+++ second_exercise/main.js
@@ -1,27 +1,38 @@
+var categories = [];
+
 $(document).ready(function () {
 
-    const request_one = {
-        "url": "https://gorest.co.in/public-api/categories",
-        "method": "GET",
-        "headers": {}
-    };
-    $.ajax(request_one).done(function (response) {
-        var data = response.data;
-        var length = response.data.length;
-        for(var i = 0; i < length; i++){
-            console.log(data[i].name);
-        }
 
-        var txtValue, input, filter;
-        input = $('#searchVal');
-        filter = input.value.toUpperCase();
+        const request_one = {
+            "url": "https://gorest.co.in/public-api/categories",
+            "method": "GET"
+        };
 
+        $.ajax(request_one).done(function (response) {
 
-        txtValue = span.textContent || searchVal().innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            show = true;
-        }
-    });
+            var filter = 'Jewelry';
+            var show;
+            filter = filter.toUpperCase();
+
+            for (var i = 0; i < response.data.length; i++) {
+                categories.push(response.data[i].name);
+                if (response.data[i].name.toUpperCase().indexOf(filter) > -1) {
+                    show = true;
+                }
+                if (show) {
+                    /*element.style.display = "";*/
+                    show = false;
+                } else {
+                    /*element.style.display = "none";*/
+                    console.log('none');
+                }
+            }
+        });
 
 
 });
+
+$(function (){
+    console.log(categories);
+    $("#search").autocomplete({source: categories});
+})
