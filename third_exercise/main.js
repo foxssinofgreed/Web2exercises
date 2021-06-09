054c757644bd92e296fd69ee378b73b312303bf3
fox <l_aptsiauri@cu.edu.ge>
2021-06-09 23:27:43 +0400

    Scoring system

--- third_exercise/main.js
+++ third_exercise/main.js
@@ -4,6 +4,8 @@ let bot_txt = $('.bot textarea')[0];
 let error_txt = $('.errors')[0];
 let correct_txt = $('.correct')[0];
 let current_txt = $('.mid textarea')[0];
+let last_total = $('.last_total')[0];
+let total = $('.total')[0];
 
 let errors = 0;
 let total_errors = 0;
@@ -15,6 +17,10 @@ let total_correct = 0;
 
 timer_txt.textContent = timeLeft + "s";
 
+let best_score = 0;
+
+total.textContent = getCookie('best_score');
+last_total.textContent = getCookie('last_total');
 
 function generateSen(){
     const settings = {
@@ -35,21 +41,18 @@ function generateSen(){
 
 function processCurrentText() {
 
-    curr_txt = current_txt.value;
-    curr_txt_arr = curr_txt.split('');
-    console.log(curr_txt_arr);
+    let curr_txt = current_txt.value;
+    let curr_txt_arr = curr_txt.split('');
 
     // get current input text and split it
-    curr_input = bot_txt.value;
-    curr_input_array = curr_input.split('');
+    let curr_input = bot_txt.value;
+    let curr_input_array = curr_input.split('');
 
     // increment total characters typed
     characterTyped++;
 
     errors = 0;
     correct_chars = 0;
-
-    console.log(curr_txt_arr);
 
     curr_txt_arr.forEach((char, index) => {
         let typedChar = curr_input_array[index];
@@ -63,9 +66,15 @@ function processCurrentText() {
 
     // display the number of errors
     error_txt.textContent = total_errors + errors;
+    console.log(errors);
 
     // update accuracy text
     correct_txt.textContent = total_correct + correct_chars;
+
+    if (best_score < correct_chars && getCookie('best_score') < correct_chars){
+        best_score = correct_chars;
+        document.cookie = 'best_score=' + best_score + ';';
+    }
 }
 
 function updateTimer() {
@@ -88,6 +97,7 @@ function start(){
 }
 function finishGame() {
     clearInterval(timer);
+    document.cookie = 'last_total=' + correct_chars + ';';
     resetValues();
 }
 
@@ -98,4 +108,24 @@ function resetValues(){
     characterTyped = 0;
     mid_txt.val('');
     $('.bot textarea').val('');
+    last_total.textContent = getCookie('last_total');
+    total.textContent = getCookie('best_score');
+}
+
+function getCookie(name) {
+    var fullName = name + "=";
+    var cookies = document.cookie.split(';');
+    for(var i = 0; i < cookies.length; i++){
+        var c = cookies[i];
+        while (c.charAt(0) === ' ') c = c.substring(1,c.length);
+        if (c.indexOf(fullName) === 0) return c.substring(fullName.length,c.length);
+    }
+    return null;
+}
+
+function clearCookies(){
+    document.cookie = 'best_score=' + 0 + ';';
+    document.cookie = 'last_total=' + 0 + ';';
+    last_total.textContent = getCookie('last_total');
+    total.textContent = getCookie('best_score');
 }
