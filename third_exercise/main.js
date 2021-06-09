cf7905ed90c965e9ee4372b66ee165adea040b7b
fox <l_aptsiauri@cu.edu.ge>
2021-06-09 23:41:01 +0400

    Minor change, fixed stupid cookie problem

--- third_exercise/main.js
+++ third_exercise/main.js
@@ -20,8 +20,8 @@ timer_txt.textContent = timeLeft + "s";
 let best_score = 0;
 
 if (getCookie('best_score') === null && getCookie('last_total') === null){
-    document.cookie = 'last_total' + 0 + ';';
-    document.cookie = 'best_score' + 0 + ';';
+    document.cookie = 'last_total=' + 0 + ';';
+    document.cookie = 'best_score=' + 0 + ';';
     total.textContent = getCookie('best_score');
     last_total.textContent = getCookie('last_total');
 }
